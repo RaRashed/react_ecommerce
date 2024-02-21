b36228270215191a7e84f80ef08b00f877c0e781
RaRAshed <rnrashedrn@gmail.com>
2024-02-21 10:55:50 -0800

    single product show and login page design

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import './CSS/LoginSignup.css'
 
 const LoginSignup = () => {
     return (
-        <div>
-            
+        <div className='loginsignup'>
+            <div className="loginsignup-container">
+                <h1>Sign Up</h1>
+                <div className='loginsignup-fields'>
+                    <input type="text" placeholder='Your name' />
+                    <input type="email" placeholder='Email Address' name="" id="" />
+                    <input type="password" placeholder='Enter Password' />
+
+                </div>
+                <button>Continue</button>
+                <p className="loginsignup-login">
+                    Already have an account  <span>Login</span>
+                </p>
+                <div className="loginsignup-agree">
+                    <input type="checkbox" name="" id="" />
+                    <p>By Continuing. I agree to the terms of use & privacy policy. </p>
+                </div>
+            </div>
+
         </div>
     );
 };
