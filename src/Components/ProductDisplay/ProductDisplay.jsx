f4f4f0e40381b949c388c947bc1a817a35779e10
RaRAshed <rnrashedrn@gmail.com>
2024-03-02 09:29:25 -0800

    product show css

--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -22,7 +22,7 @@ const ProductDisplay = (props) => {
                 <h1>
                     {product.name}
                 </h1>
-                <div className="productdisplay-right-star">
+                <div className="productdisplay-right-stars">
                     <img src={star_icon} alt="" />
                     <img src={star_icon} alt="" />
                     <img src={star_icon} alt="" />
