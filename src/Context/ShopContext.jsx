58d8d53149d0f39b484f71a69a83d1017bdbe6db
RaRAshed <rnrashedrn@gmail.com>
2024-03-12 12:11:22 -0700

    cart item view

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -14,14 +14,23 @@ const getDefaultCart = () => {
 const ShopContextProvider = (props) => {
     const [cartItems, setCartItems] = useState(getDefaultCart());
     const addToCart = (itemId) => {
-        console.log(itemId);
         setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
-        console.log(cartItems);
     }
     const removeFromCart = (itemId) => {
         setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
     }
-    const contextValue = { all_product, cartItems, addToCart, removeFromCart };
+   const getTotalCartAmount = () => {
+    let totalAmount = 0;
+    Object.keys(cartItems).forEach(item => {
+        if (cartItems[item] > 0) {
+            let itemInfo = all_product.find(product => product.id === Number(item));
+            totalAmount += itemInfo.new_price * cartItems[item];
+        }
+    });
+    return totalAmount;
+}
+
+    const contextValue = { getTotalCartAmount ,all_product, cartItems, addToCart, removeFromCart };
 
     return (
         <ShopContext.Provider value={contextValue}>
